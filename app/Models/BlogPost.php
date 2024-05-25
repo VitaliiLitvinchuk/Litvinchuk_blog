5763bebcc125bc9e544e452038887d826e2cf5b7
VitaliiLitvinchuk <vitalii.litvinchuk@oa.edu.ua>
2024-05-25 13:01:26 +0300

    lab 8

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -4,10 +4,47 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+
+    protected $fillable
+        = [
+            'title',
+            'slug',
+            'category_id',
+            'excerpt',
+            'content_raw',
+            'is_published',
+            'published_at',
+            'user_id',
+        ];
+
+    /**
+     * Категорія статті
+     *
+     * @return BelongsTo
+     */
+    public function category()
+    {
+        //стаття належить категорії
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    /**
+     * Автор статті
+     *
+     * @return BelongsTo
+     */
+    public function user()
+    {
+        //стаття належить користувачу
+        return $this->belongsTo(User::class);
+    }
+
 }
